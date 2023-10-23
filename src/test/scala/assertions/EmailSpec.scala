cd0030236075a6201f5cb5b39a9ac7b90ca88c0d
adamdavis99 <aruneshsarker1970@gmail.com>
2023-10-24 01:05:46 +0530

    "Added clue for assert, assertResult and assume"

--- src/test/scala/assertions/EmailSpec.scala
+++ src/test/scala/assertions/EmailSpec.scala
@@ -8,19 +8,21 @@ class EmailSpec extends AnyFlatSpec{
 
   it should "return an Email object for a valid string" in {
     val email = Email("arunesh.sarker@workday.com")
-    assert(email.localPart === "arunesh.sarker")
+    assert(email.localPart === "arunesh.sarker", "Local part should be arunesh.sarker")
     assert(email.domain === "workday.com")
   }
 
   it should "return another Email object for another valid string" in {
-    assertResult("jim") {
+    assertResult("jim", "Expected local part is 'jim'") {
       Email("jim@workday.com").localPart
     }
   }
 
   it should "throw an exception when an email does not contain @ symbol" in {
-    assertThrows[IllegalArgumentException] {
-      Email("jim.com")
+    withClue("Expected exception since email does not have '@'") {
+      assertThrows[IllegalArgumentException] {
+        Email("jim.com")
+      }
     }
   }
 
