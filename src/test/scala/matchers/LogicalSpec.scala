f6b559c1181bc18de2677735952869884b459038
adamdavis99 <aruneshsarker1970@gmail.com>
2023-10-30 19:17:53 +0530

    Cretaed LogicalSpec class which teaches us to test for logical conditions like and | or

---
+++ src/test/scala/matchers/LogicalSpec.scala
@@ -0,0 +1,20 @@
+package matchers
+
+import com.h2.services.Currency
+
+class LogicalSpec extends UnitSpec {
+  behavior of "Currencies as logical and | or"
+
+  it should "successfully match logical expression with 'and' condition for a currency" in {
+    val tenNZD : Currency = "10 NZD"
+    tenNZD.costInDollars.amount should (be > 0 and be <= 10)
+  }
+
+  it should "successfully match logical expression with 'or' condition for a currency" in {
+    val tenNZD : Currency = "10 NZD"
+    tenNZD.code should (have length 0 or equal ("NZD"))
+  }
+
+
+
+}
